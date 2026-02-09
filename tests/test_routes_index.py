07a30bc05b8aeb58bc32ebc38df183306e05fb81
David de Klerk <david.deklerk@ed.ac.uk>
2026-02-09 10:37:46 +0000

    Fix unit tests

--- tests/test_routes_index.py
+++ tests/test_routes_index.py
@@ -11,7 +11,7 @@ def test_index(client):
     response = client.get("/")
 
     assert response.status_code == 200
-    assert b"<div id='map' class='map'></div>" in response.data
+    assert b'<div id="map" class="map" tabindex="0"></div>' in response.data
     assert b"src=\"/static/dist/main.js\"" in response.data
 
 
